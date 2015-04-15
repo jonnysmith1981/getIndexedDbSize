1e5a8416b4600e616f51bceff5aefd28ff323e1d
Jonathan <jonathan@alpine-software.co.uk>
2015-04-15 01:09:48 +0100

    Fixed issue where parameter "dbname" was incorrectly being passed into function.

--- getIndexedDbSize.js
+++ getIndexedDbSize.js
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-(function showIndexedDbSize(dbname) {
+(function showIndexedDbSize() {
   "use strict";
   var db;
   var storesizes = new Array();
